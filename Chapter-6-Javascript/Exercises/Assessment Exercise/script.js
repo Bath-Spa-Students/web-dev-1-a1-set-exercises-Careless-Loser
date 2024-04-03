9d723a06083b88e82c77b8d73be292ba1065337f
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2024-04-03 14:56:02 +0400

    Update script.js

--- Chapter-6-Javascript/Exercises/Assessment Exercise/script.js
+++ Chapter-6-Javascript/Exercises/Assessment Exercise/script.js
@@ -1,5 +1,5 @@
-function calprice(){
+document.getElementById('Total').addEventListener('click', function() {
     var price = document.getElementById('Price').value;
     var ltr = document.getElementById('liter').value;
-    document.getElementById('response').innerHTML = "Total Price = " + (parseFloat(price)*parseFloat(ltr));
-}
+    document.getElementById('response').innerHTML = "Total Price = " + (parseFloat(price) * parseFloat(ltr));
+});
